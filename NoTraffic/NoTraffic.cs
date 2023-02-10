e6720986efb31ab8c3cc7a92f0e3082cf2b703ea
salgue441 <carlossalguero441@gmail.com>
2023-02-10 16:11:20 -0600

    Fixed Array out of bounds, working on no events

--- NoTraffic/NoTraffic.cs
+++ NoTraffic/NoTraffic.cs
@@ -30,25 +30,69 @@ namespace NoTraffic.Source
             {
                 Vehicle vehicle = instance.m_vehicles.m_buffer[i];
 
-                if (vehicle.m_flags != Vehicle.Flags.Spawned)
+                // If array is empty, skip
+                if (vehicle.m_flags == 0)
                 {
-                    instance.ReleaseVehicle((ushort)i);
+                    continue;
                 }
+
+                // If array index is out of bounds, skip
+                if (i >= numVehicles)
+                {
+                    continue;
+                }
+
+
+                if (vehicle.m_flags == Vehicle.Flags.Created &&
+                    vehicle.m_flags == Vehicle.Flags.Spawned)
+                {
+                    continue;
+                }
+
+                // Set the flags to 0
+                instance.m_vehicles.m_buffer[i].m_flags = 0;
+
+                // Set the vehicle to despawn
+                instance.m_vehicles.m_buffer[i].m_flags = Vehicle.Flags.WaitingPath;
             }
+        }
+    }
 
-            if (numVehicles == 0)
+    /** 
+     * Stop the crime, fire, ambulance, police, and garbage trucks events from spawning
+     */
+    public class NoEvents : ThreadingExtensionBase
+    {
+        /**
+         * @brief
+         * Gets the instance of the EventManager and the number of events in the
+         * game. Then it loops through all the events and sets their flags to 0
+         * @param realTimeDelta The time since the last update.
+         * @param simulationTimeDelta The time since the last simulation update.
+         */
+        public override void OnUpdate(float realTimeDelta, float simulationTimeDelta)
+        {
+            EventManager instance = Singleton<EventManager>.instance;
+            int numEvents = instance.m_events.m_size;
+
+            for (int i = 0; i < numEvents; i++)
             {
-                numVehicles = (uint)instance.m_vehicles.m_buffer.Length;
+                EventData evt = instance.m_events.m_buffer[i];
 
-                for (int i = 0; i < numVehicles; i++)
+                // If array is empty, skip
+                if (evt.m_flags == 0)
                 {
-                    Vehicle vehicle = instance.m_vehicles.m_buffer[i];
-
-                    if (vehicle.m_flags != Vehicle.Flags.Spawned)
-                    {
-                        instance.ReleaseVehicle((ushort)i);
-                    }
+                    continue;
                 }
+
+                // If array index is out of bounds, skip
+                if (i >= numEvents)
+                {
+                    continue;
+                }
+
+                // Set the flags to 0
+                instance.m_events.m_buffer[i].m_flags = 0;
             }
         }
     }
