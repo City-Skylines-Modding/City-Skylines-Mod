f6ce3706c3fd80a88eb0212bf4da14e0fdc04fbb
salgue441 <carlossalguero441@gmail.com>
2023-02-11 15:56:11 -0600

    Cambios en el codigo fuente

--- NoTraffic/NoTraffic.cs
+++ NoTraffic/NoTraffic.cs
@@ -11,48 +11,87 @@ namespace NoTraffic.Source
         public string Description => "Stop Vehicle generation in city skylines";
     }
 
+    public class NoTrafficLoader : LoadingExtensionBase
+    {
+        public NoTraffic noTraffic;
+
+        /**
+         * @brief
+         * Overrides the default behaviour of the LoadingExtensionBase function.
+         * Allows the mod to be accessed when a game is loaded or created.
+         * @param mode Mode in which the game is accessed.
+         */
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            if (mode == LoadMode.LoadGame || mode == LoadMode.NewGame)
+            {
+                noTraffic = new NoTraffic();
+            }
+
+            base.OnLevelLoaded(mode);
+        }
+
+        /**
+         * @brief
+         * Overrides the default behaviour of the OnLevelUnloading function.
+         * Deletes the mod when the game is closed.
+         */
+        public override void OnLevelUnloading()
+        {
+            noTraffic = null;
+            base.OnLevelUnloading();
+        }
+    }
+
     public class NoTraffic : ThreadingExtensionBase
     {
         /**
          * @brief
-         * Gets the instance of the VehicleManager and the number of vehicles in the
-         * game. Then it loops through all the vehicles and sets their flags to 0
+         * Overrides the default behavious of the OnUpade Function associated 
+         * with the ThreadingExtensionBase class.
          * @param realTimeDelta The time since the last update.
          * @param simulationTimeDelta The time since the last simulation update.
          */
         public override void OnUpdate(float realTimeDelta, float simulationTimeDelta)
         {
-            VehicleManager instance = Singleton<VehicleManager>.instance;
-            uint numVehicles = instance.m_vehicles.m_size;
+            DestroyVehicles();
+            base.OnUpdate(realTimeDelta, simulationTimeDelta);
+        }
+        
+        /**
+         * @brief
+         * Gets the instance of the VehicleManager and the number of vehicles in the game.
+         * Then, it loops through all the vehicles and sets their flags to 0.
+         * @return Returns if the vehicle array size is 0.
+         */
+        private void DestroyVehicles()
+        {
+            VehicleManager vehicleInstance = Singleton<VehicleManager>.instance;
+            uint numVehicles = vehicleInstance.m_vehicles.m_size;
+
+            if (numVehicles == 0)
+            {
+                return;
+            }
 
             for (int i = 0; i < numVehicles; i++)
             {
-                Vehicle vehicle = instance.m_vehicles.m_buffer[i];
+                Vehicle vehicles = vehicleInstance.m_vehicles.m_buffer[i];
 
-                // If array is empty, skip
-                if (vehicle.m_flags == 0)
+                // If array is empty
+                if (vehicles.m_flags == 0)
                 {
                     continue;
                 }
 
-                // If array index is out of bounds, skip
                 if (i >= numVehicles)
                 {
-                    continue;
+                    break;
                 }
 
-
-                if (vehicle.m_flags == Vehicle.Flags.Created &&
-                    vehicle.m_flags == Vehicle.Flags.Spawned)
-                {
-                    continue;
-                }
-
-                // Set the flags to 0
-                instance.m_vehicles.m_buffer[i].m_flags = 0;
-
-                // Set the vehicle to despawn
-                instance.m_vehicles.m_buffer[i].m_flags = Vehicle.Flags.WaitingPath;
+                // Set flags to 0 and remove vehicle
+                vehicleInstance.m_vehicles.m_buffer[i].m_flags = 0;
+                vehicleInstance.m_vehicles.m_buffer[i].m_flags = Vehicle.Flags.WaitingPath;
             }
         }
     }
